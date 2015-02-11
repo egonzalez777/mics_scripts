357c764ca2ec785c859902020bb0c4c3eb070d6c
Eric Gonzalez <eric.gonz.gomez@gmail.com>
2015-02-10 19:47:28 -0800

    Adding percolation V1, and a sample script.

--- qu/percolation.py
+++ qu/percolation.py
@@ -1,31 +1,48 @@
-from
-from time import time
+import random
+import pprint
 
 
 class Percolate(object):
-    grid = [][]
+    N = None
+    grid = None
+    i = set()
 
     def __init__(self, N):
-        self.grid = [0]
+        self.N = N
+        self.grid = [[0 for k in [0]*N] for i in [0]*N]
+
+        self.c = 0
+        rand = random.randint(0, 10)
+        n = N*N/rand
+        print '-----', n, '--------'
+        while True:
+            i = random.randint(0, N-1)
+            j = random.randint(0, N-1)
+            self.union(i, j)
+            self.i.add((i, j))
+            self.c = self.c + 1
+            if self.c == n:
+                break
 
     def connected(self, i, j):
         return True
 
     def union(self, i, j):
-        pass
+        self.grid[i][j] = 1
 
     def percolates(self):
-        pass
+        return self.N**2 - float(self.c) < self.c
 
 
 
 def main(i):
     p = Percolate(i)
-    print p.grid
-    count = 0
-    tries =
+    pp = pprint.PrettyPrinter()
 
-    while count <
+    pp.pprint(p.grid)
+    print(p.i)
+    print(p.percolates())
+
 
 if __name__ == '__main__':
     import sys
